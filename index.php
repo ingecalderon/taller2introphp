0bda9214c49c2fbd0c5f9ecf9509a9d341bdfec7
Edwin Calderon <ingecalderon@hotmail.com>
2020-11-16 03:00:04 -0500

    Taller 2 Introduccion a php usando tablas, listas y css

---
+++ index.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Taller 2 - Introducción a php</title>
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+
+<body>
+	<!-- Este es el Titulo de Pagina -->
+	<h1> TALLER 2 INTRODUCCION A PHP </h1>
+	<br>
+	<!-- Punto 1. Elaborar una página HTML que por medio de manejo de estilos CSS tenga un aspecto similar al siguiente: -->
+	<!-- Este es el tercer Titulo -->
+	<h2> 1. Elaborar una página HTML que por medio de manejo de estilos CSS tenga un aspecto similar al siguiente: </h2>
+	<table>
+		<tr>
+			<td class="infoizq">
+				<p>Edwin Calderon</p>
+			</td>
+			<td class="infoder">
+				<p>ingecalderon@hotmail.com</p>
+			</td>
+		</tr>
+	</table>
+	<table id="tablacuerpo">
+		<tr>
+			<!-- Celdas Vacias -->
+			<td class="cuerpo1"></td>
+			<td class="cuerpo2"></td>
+
+			<!-- Celda Con Contenido -->
+			<td class="cuerpo3">
+				<!--Este es el primer Titulo -->
+				<h2> Objetivo </h2>
+				<!--Este es el primer Parrafo -->
+				<p>Posicionarme como ingeniero de sistemas.</p>
+				<p><br></p>
+
+				<!--Este es el segundo Titulo -->
+				<h2> Experiencia </h2>
+				<!-- Este es el segundo Parrafo -->
+				<p>Desarrollador Junior, Empresa de Software (2010 &ndash; A la fecha)</p>
+				<ul>
+					<li>Diseño e implementación de funcionalidades para usuarios finales de producto líder</li>
+					<li>Escribir bibliotecas JavaScript y Ruby de terceros</li>
+				</ul>
+				<p><br></p>
+
+				<!-- Este es el tercer Titulo -->
+				<h2> Habilidades </h2>
+				<!-- Este es el tercer Parrafo -->
+				<p>Lenguajes C#, JavaScript, Python, Ruby<br>
+					Framework .Net, Node.js, Django, Ruby on Rails</p>
+				<p><br></p>
+
+				<!--Este es el cuarto Titulo -->
+				<h2>Estudios</h2>
+				<!-- Este es el cuarto Parrafo -->
+				<p>TECNICO LABORAL EN ASISTENTE EN ANALISIS Y DESARROLLO DE SOFTWARE, CENSA</p>
+				<p><br></p>
+				<ul>
+					<li>Participación destacada en feria de emprendimiento y tecnología</li>
+					<li>Promedio Académico: 4.9</li>
+				</ul>
+			</td>
+		</tr>
+		</td>
+		</tr>
+		<br>
+	</table>
+	<br>
+	<table>
+		<tr>
+			<td class="pie">
+				<p>Introduccion a php - Medellin 2020</p>
+			</td>
+		</tr>
+	</table>
+	<p><br></p>
+	<!-- Punto 2. Diseño de una tabla -->
+	<!-- Este es el tercer Titulo -->
+	<h2> 2. Diseñar la siguiente tabla: </h2>
+	<table id="punto2">
+		<tr>
+			<td></td>
+			<td class="punto2" colspan="2">Fernando<br></td><br>
+			<td class="punto2" colspan="2">Cecilia</td>
+		</tr>
+		<tr>
+			<td class="punto2"><b>Fecha</b></td>
+			<td class="punto2">Ventas</td>
+			<td class="punto2">Gastos</td>
+			<td class="punto2">Ventas</td>
+			<td class="punto2">Gastos</td>
+		</tr>
+		<tr>
+			<td class="punto2"><b>20/01</b></td>
+			<td class="punto2num">650000</td>
+			<td class="punto2num">50000</td>
+			<td class="punto2num">750000</td>
+			<td class="punto2num">55000</td>
+		</tr>
+		<tr>
+			<td class="punto2"><b>22/01</b></td>
+			<td class="punto2num">750000</td>
+			<td class="punto2num">60000</td>
+			<td class="punto2num">650000</td>
+			<td class="punto2num">60000</td>
+		</tr>
+		<tr>
+			<td class="punto2"><b>26/01</b></td>
+			<td class="punto2num">900000</td>
+			<td class="punto2num">45000</td>
+			<td class="punto2num">950000</td>
+			<td class="punto2num">65000</td>
+		</tr>
+		<tr>
+			<td class="punto2"><b>30/01</b></td>
+			<td class="punto2num">1100000</td>
+			<td class="punto2num">50000</td>
+			<td class="punto2num">1000000</td>
+			<td class="punto2num">60000</td>
+		</tr>
+		<tr>
+			<td class="punto2"><b>04/02</b></td>
+			<td class="punto2num">850000</td>
+			<td class="punto2num">65000</td>
+			<td class="punto2num">750000</td>
+			<td class="punto2num">65000</td>
+		</tr>
+		<tr>
+			<td class="punto2"><b>Total</b></td>
+			<td class="punto2num"><b>4250000</b></td>
+			<td class="punto2num"><b>270000</b></td>
+			<td class="punto2num"><b>4100000</b></td>
+			<td class="punto2num"><b>305000</b></td>
+		</tr>
+	</table>
+
+</body>
+
+</html>
